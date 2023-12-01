5020ef10dc63cd208f1f320a3bf7f3d9f1562040
anhnmt <anhng.dev@gmail.com>
2023-12-01 23:58:10 +0700

    Update siteMetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -55,17 +55,17 @@ const siteMetadata = {
         giscusConfig: {
             // Visit the link below, and follow the steps in the 'configuration' section
             // https://giscus.app/
-            repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-            repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-            category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-            categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+            repo: process.env.NEXT_PUBLIC_GISCUS_REPO || 'anhnmt/anhnmt.github.io',
+            repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID || 'R_kgDOKi692Q',
+            category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY || 'General',
+            categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID || 'DIC_kwDOKJlg-c4CYwO3',
             mapping: 'pathname', // supported options: pathname, url, title
             reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
             // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
             metadata: '0',
             // theme example: light, dark, dark_dimmed, dark_high_contrast
             // transparent_dark, preferred_color_scheme, custom
-            theme: 'light',
+            theme: 'dark',
             // theme when dark mode
             darkTheme: 'transparent_dark',
             // If the theme option above is set to 'custom`
