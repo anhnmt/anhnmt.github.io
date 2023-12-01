3292b6dc82a7e0e6dea35c98a86bed2243d74207
anhnmt <anhng.dev@gmail.com>
2023-12-02 00:04:22 +0700

    Update siteMetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -26,10 +26,10 @@ const siteMetadata = {
         // If you want to use an analytics provider you have to add it to the
         // content security policy in the `next.config.js` file.
         // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-        umamiAnalytics: {
-            // We use an env variable for this site to avoid other users cloning our analytics ID
-            umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-        },
+        // umamiAnalytics: {
+        //     // We use an env variable for this site to avoid other users cloning our analytics ID
+        //     umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+        // },
         // plausibleAnalytics: {
         //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
         // },
@@ -40,11 +40,6 @@ const siteMetadata = {
         // googleAnalytics: {
         //   googleAnalyticsId: '', // e.g. G-XXXXXXX
         // },
-    },
-    newsletter: {
-        // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
-        // Please add your .env file and modify it according to your selection
-        provider: 'mailchimp',
     },
     comments: {
         // If you want to use an analytics provider you have to add it to the
