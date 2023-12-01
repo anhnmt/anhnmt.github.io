d295ffae60c0989abe777a91acb908d260271b10
anhnmt <anhng.dev@gmail.com>
2023-12-01 21:34:21 +0700

    Update metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,25 +1,25 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: "Prabhu's Blog",
-  author: 'Prabhu Kiran Konda',
-  headerTitle: "Prabhu's Blog",
+  title: "Anh Nguyen's Blog",
+  author: 'Anh Nguyen aka AnhNMT',
+  headerTitle: "Anh Nguyen's Blog",
   description: 'Exploring Tech and more',
   language: 'en-us',
   theme: 'dark', // system, dark or light
-  siteUrl: 'https://prabhukirankonda.vercel.app',
-  siteRepo: 'https://github.com/PrabhuKiran8790/prabhukirankonda',
+  siteUrl: 'https://anhnmt.dev',
+  siteRepo: 'https://github.com/anhnmt/anhnmt.github.io',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'prabhukiran426@gmail.com',
-  github: 'https://github.com/PrabhuKiran8790',
-  twitter: 'https://twitter.com/prabhukirantwt',
-  linkedin: 'https://www.linkedin.com/in/prabhukirankonda',
+  // email: 'anhng.dev@gmail.com',
+  github: 'https://github.com/anhnmt',
+  // twitter: 'https://twitter.com/prabhukirantwt',
+  // linkedin: 'https://www.linkedin.com/in/prabhukirankonda',
   locale: 'en-US',
   socialLinks: {
     github: 'https://www.github.com/prabhukiran8790',
-    linkedin: 'https://www.linkedin.com/in/prabhukirankonda',
-    twitter: 'https://twitter.com/prabhukirantwt',
-    mail: 'mailto:prabhukiran426@gmail.com',
+    // linkedin: 'https://www.linkedin.com/in/prabhukirankonda',
+    // twitter: 'https://twitter.com/prabhukirantwt',
+    // mail: 'mailto:prabhukiran426@gmail.com',
   },
   analytics: {
     // If you want to use an analytics provider you have to add it to the
